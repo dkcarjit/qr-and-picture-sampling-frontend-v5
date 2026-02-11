3d09f88bcc2751b163f1396e6fe41d445986d389
dkcvivek <vivek.jha@dkcexport.co.in>
2026-02-11 12:21:18 +0530

    Fixed footer

--- app/components/ui/Footer.tsx
+++ app/components/ui/Footer.tsx
@@ -1,10 +1,15 @@
 import { QrCode } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const Footer = () => {
+  const router= useRouter();
+
   return (
-    <footer className="h-12 bg-blue-500 text-white flex items-center justify-center gap-2">
-      <span className="font-semibold tracking-wide">SCAN</span>
-      <QrCode size={20} />
+    <footer className="h-12 bg-blue-500 text-white positon-fixed bottom-0 left-0 right-0">
+      <div className="flex items-center justify-center gap-2 h-full" onClick={()=>router.push("/")}>
+        <span className="font-semibold tracking-wide">SCAN</span>
+        <QrCode size={20} />
+      </div>
     </footer>
   );
 };
